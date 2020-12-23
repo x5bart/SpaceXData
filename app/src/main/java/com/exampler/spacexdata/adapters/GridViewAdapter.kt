c15416cd7e4c601b0b66a9951d7bf36acc9e9e3f
Pavel Kovalchuk <x5bart@yandex.ru>
2020-12-23 16:29:51 +0300

    add save image to gallery

--- app/src/main/java/com/exampler/spacexdata/adapters/GridViewAdapter.kt
+++ app/src/main/java/com/exampler/spacexdata/adapters/GridViewAdapter.kt
@@ -1,6 +1,7 @@
 package com.exampler.spacexdata.adapters
 
 import android.content.Context
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,9 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.annotation.GlideModule
 import com.exampler.spacexdata.R
+import com.exampler.spacexdata.activity.MainActivity
+import com.exampler.spacexdata.fragments.DetailFragment
+import com.exampler.spacexdata.fragments.ImageFragment
 
 @GlideModule
 class GridViewAdapter(private val ctx: Context, private val imageUrls: ArrayList<String>) :
@@ -23,7 +27,22 @@ class GridViewAdapter(private val ctx: Context, private val imageUrls: ArrayList
         Glide
             .with(ctx)
             .load(imageUrls[position])
+            .placeholder(R.drawable.ic_logo_s)
             .into(imageView)
+
+        convertView.setOnClickListener {
+            val fragment = ImageFragment()
+            val bundle = Bundle()
+            val url = imageUrls[position]
+            bundle.putString("img", url)
+
+            fragment.arguments = bundle
+            val fragmentManager =
+                (context as MainActivity).supportFragmentManager.beginTransaction()
+            fragmentManager.addToBackStack("detail")
+            fragmentManager.replace(R.id.fragment_container, fragment)
+            fragmentManager.commit()
+        }
         return convertView
     }
 
---
+++ app/src/main/java/com/exampler/spacexdata/fragments/ImageFragment.kt
@@ -0,0 +1,218 @@
+package com.exampler.spacexdata.fragments
+
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.os.Handler
+import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.exampler.spacexdata.R
+import com.exampler.spacexdata.activity.TAG
+import com.exampler.spacexdata.databinding.FragmentImageBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+
+
+class ImageFragment : Fragment() {
+    private val hideHandler = Handler()
+    private lateinit var image: String
+    private lateinit var binding: FragmentImageBinding
+    private var dummyButton: Button? = null
+    private var fullscreenContent: View? = null
+    private var fullscreenContentControls: View? = null
+
+    @Suppress("InlinedApi")
+    private val hidePart2Runnable = Runnable {
+        val flags =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        activity?.window?.decorView?.systemUiVisibility = flags
+        (activity as? AppCompatActivity)?.supportActionBar?.hide()
+    }
+    private val showPart2Runnable = Runnable {
+        // Delayed display of UI elements
+        fullscreenContentControls?.visibility = View.VISIBLE
+    }
+    private var visible: Boolean = false
+    private val hideRunnable = Runnable { hide() }
+
+
+    private fun saveMediaToStorage() {
+        val bitmap = binding.fullscreenContent.drawable.toBitmap()
+        val filename = "${System.currentTimeMillis()}.jpg"
+        var outputStream: OutputStream? = null
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            context?.contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                outputStream = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            outputStream = FileOutputStream(image)
+        }
+
+        outputStream?.use {
+            //Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(this.activity, "image saved", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentImageBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        visible = true
+
+        dummyButton = view.findViewById(R.id.dummy_button)
+        fullscreenContent = view.findViewById(R.id.fullscreen_content)
+        fullscreenContentControls = view.findViewById(R.id.fullscreen_content_controls)
+        // Set up the user interaction to manually show or hide the system UI.
+        fullscreenContent?.setOnClickListener { toggle() }
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        dummyButton?.setOnClickListener {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS)
+            }
+            saveMediaToStorage()
+        }
+
+
+
+        image = arguments!!.getString("img", "")
+
+        Glide.with(this.activity!!)
+            .load(image)
+            .into(binding.fullscreenContent)
+
+        Log.d(TAG, "image: $image")
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        // Clear the systemUiVisibility flag
+        activity?.window?.decorView?.systemUiVisibility = 0
+        show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        dummyButton = null
+        fullscreenContent = null
+        fullscreenContentControls = null
+    }
+
+    private fun toggle() {
+        if (visible) {
+            hide()
+        } else {
+            show()
+        }
+    }
+
+    private fun hide() {
+        // Hide UI first
+        fullscreenContentControls?.visibility = View.GONE
+        visible = false
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        hideHandler.removeCallbacks(showPart2Runnable)
+        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    @Suppress("InlinedApi")
+    private fun show() {
+        // Show the system bar
+        fullscreenContent?.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        visible = true
+
+        // Schedule a runnable to display UI elements after a delay
+        hideHandler.removeCallbacks(hidePart2Runnable)
+        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY.toLong())
+        (activity as? AppCompatActivity)?.supportActionBar?.show()
+    }
+
+    /**
+     * Schedules a call to hide() in [delayMillis], canceling any
+     * previously scheduled calls.
+     */
+    private fun delayedHide(delayMillis: Int) {
+        hideHandler.removeCallbacks(hideRunnable)
+        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
+    }
+
+    companion object {
+        /**
+         * Whether or not the system UI should be auto-hidden after
+         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+         */
+        private const val AUTO_HIDE = true
+
+        /**
+         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+         * user interaction before hiding the system UI.
+         */
+        private const val AUTO_HIDE_DELAY_MILLIS = 3000
+
+        /**
+         * Some older devices needs a small delay between UI widget updates
+         * and a change of the status and navigation bar.
+         */
+        private const val UI_ANIMATION_DELAY = 300
+    }
+}
